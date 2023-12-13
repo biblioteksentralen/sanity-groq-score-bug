20a6aa2ebfbef9a04f1c6585f6e30c1a269d47e5
Daniel Winsvold <danielwinsvold@gmail.com>
2023-12-13 09:16:38 +0100

    add simple schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,23 @@
-export const schemaTypes = []
+import {defineType} from 'sanity'
+
+const book = defineType({
+  name: 'book',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+    },
+    {
+      name: 'subtitle',
+      type: 'string',
+    },
+    {
+      name: 'authors',
+      type: 'array',
+      of: [{type: 'string'}],
+    },
+  ],
+})
+
+export const schemaTypes = [book]
